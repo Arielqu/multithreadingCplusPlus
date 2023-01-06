f6e0ab4c0790dfce790710f43c59992fda28781e
quhuajif <quhuajif@u86d7610a9ff852.ant.amazon.com>
2023-01-06 16:09:21 -0500

    atomic types

--- 06_atomicTypes.cpp
+++ 06_atomicTypes.cpp
@@ -1,4 +1,8 @@
 #include<iostream>
+#include<thread>
+#include<vector>
+#include<mutex>
+#include<atomic>
 using namespace std;
 
 /*
@@ -12,6 +16,17 @@ data copied from RAM into register to do operation and copy back to RAM
 increment value in the processor register; update the count variable in RAM 
 atomic variables 
 can declare count as atomic, prevents data race 
+exercise 1: wrtie program run task 10 concurrent threads;
+• A data race occurs, because multiple threads access the count variable, at
+least one thread modifies it, and there is no "happens before" relationship
+between the thread accesses
+• Even though incrementing an integer is an uninterruptible operation, the
+value of the variable needs to be fetched from RAM and stored there, and
+threads can be interrupted between these operations
+• This causes threads to interfere with each other when accessing the count
+variable, which causes the answer to be wrong
+
+exercise 2: avoid the data race with a mutex [Done]
 
 
 **** atomic types ****
@@ -23,6 +38,17 @@ x =2; y = x;
 volatile keyword
 the value may be chane without explictly modified; typically used when accessing hardware; 
 prevents compiler optimizations which assumes the value does not change 
+exercise 3: use atomic variable type 
+• Non-atomic version
+• There is a data race. Threads interfere with each other and use incorrect
+values when incrementing the counter
+• Atomic version
+• Incrementing the counter is done as an uninterruptible operation and the
+new value of the counter is immediately visible to all threads. This ensures
+the correct result
+• Volatile version
+• The volatile keyword has no significance in multi-threaded C++ programs. This
+is equivalent to the non-atomic version
 
 **** atomic operations ****
 atomic member functions 
@@ -54,7 +80,7 @@ acquire and release semantis
     read flag as clear-> acquire access
     set the flag as clear -> release access; release the result of modification; next thread reads the flag as clear will acquire the modificaiton
 impose an ordering on access to the variable -> no data race 
-
+exercise 4: function of spin lock 
 
 **** lock-free programming ****
 lock free programming allow threads to execute critical sections without assistance from the operating system locking facilities 
@@ -71,3 +97,44 @@ double-checked locking
     sp->do_it();
 
 */
+//exercise 1: concurrency task 
+// int counter{0};
+//exercise 3: atomic type
+// atomic<int> counter{0};
+//if volatile
+volatile int counter{0};
+mutex mt;
+void task(){
+    for (int i=0; i<100'000; ++i){
+        // lock_guard<mutex> lg(mt);
+        // mt.lock();
+        ++counter;
+        // mt.unlock();
+    }
+}
+
+//exercise 4: spin lock 
+atomic_flag af = ATOMIC_FLAG_INIT;
+void taskSpin(int n){
+    while (af.test_and_set()) {}
+    this_thread::sleep_for(50ms);
+    cout << "I am a task with argument " << n <<endl;
+    af.clear();
+}
+int main(){
+    vector<thread> threads;
+    // for (int i=0; i<10; ++i){
+    //     threads.push_back(thread{task});
+    // }
+    // for (auto& thread : threads)
+    //     thread.join();
+
+    //exercise 4: spin lock 
+    for (int i=0; i<10; i++){
+        threads.push_back(thread{taskSpin,i});
+    }
+    for (auto& thread : threads)
+        thread.join();
+    cout <<"the conunter is : " << counter <<endl;
+    return 0;
+}
